cd50762579501bc2a0cdfad7d2cf9eb3138623c6
Chris Haumesser <ch@wryfi.net>
2015-03-17 17:11:16 -0700

    tweaks and tests for calling from shell

--- rpncalc.py
+++ rpncalc.py
@@ -7,7 +7,7 @@ import sys
 logging.basicConfig(
     format='%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 
 calculationExpressions = {
@@ -20,7 +20,12 @@ calculationExpressions = {
 
 
 def main(expression):
-  calculator(expression)
+  if not re.match(r'[0-9*/+-]+', expression):
+    raise SystemExit('Error: RPN expressions can only contain numbers and operators.')
+  try:
+    print(calculator(expression))
+  except RuntimeError as error:
+    raise SystemExit(error)
 
 
 def calculator(expression):
@@ -32,7 +37,7 @@ def calculator(expression):
   elements = []
   for component in parsed:
     # processing below does not check for missing spaces
-    if re.match(r'\d*\.*\d+', component):
+    if re.match(r'^\d*\.*\d+$', component):
       elements.append(Decimal(component))
     elif component in calculationExpressions.keys():
       elements.append(component)
@@ -43,17 +48,19 @@ def calculator(expression):
       stack.append(element)
     else:
       if len(stack) < 2:
-        raise RuntimeError('Not enough values')
+        raise RuntimeError('There were not enough operands in the expression.')
       else:
         operands = [stack.pop()]
         operands.insert(0, stack.pop())
         result = calculationExpressions.get(element)(operands[0], operands[1])
         stack.append(result)
   if len(stack) > 1:
-    raise RuntimeError('There were not enough elements in the expression.')
+    raise RuntimeError('There were not enough operators in the expression.')
   logging.debug(stack)
   return stack.pop()
 
 
 if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    raise SystemExit('Specify an RPN expression in quotes.')
   main(sys.argv[1])
--- tests.py
+++ tests.py
@@ -1,32 +1,81 @@
-from decimal import getcontext, Decimal
+from decimal import Decimal
+import os
+import subprocess
 import unittest
 
 TEST_EXPRESSIONS = [
-    ('igg_one', '1 2 +', '3'),
-    ('igg_two', '4 2 /', '2'),
-    ('igg_three', '2 3 4 + *', '14'),
-    ('igg_four', '3 4 + 5 6 + *', '77'),
-    ('igg_five', '13 4 -', '9'),
+    ('igg_expr_one', '1 2 +', '3'),
+    ('igg_expr_two', '4 2 /', '2'),
+    ('igg_expr_three', '2 3 4 + *', '14'),
+    ('igg_expr_four', '3 4 + 5 6 + *', '77'),
+    ('igg_expr_five', '13 4 -', '9'),
 ]
 
 
 class RpnTest(unittest.TestCase):
 
   def test_can_import_rpncalc(self):
+    '''
+    Tests that we can import the calculator function from the rpncalc module.
+    '''
     try:
       from rpncalc import calculator
     except ImportError:
       self.fail('Could not import calculator')
 
+  def test_command(self):
+    '''
+    Tests that the module can be called from the shell
+    '''
+    if os.path.isfile('rpncalc.py'):
+      command = subprocess.Popen(['python', 'rpncalc.py', '4 2 /'], stdout=subprocess.PIPE)
+      response = command.communicate()[0].decode()
+      self.assertEqual(Decimal('2'), Decimal(response))
+    else:
+      fail('rpncalc.py not found.')
+
+  def test_command_igg_err1(self):
+    '''
+    Tests indiegogo error scenario one.
+    '''
+    if os.path.isfile('rpncalc.py'):
+      command = subprocess.Popen(
+          ['python', 'rpncalc.py', '1 +'],
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE
+      )
+      response = command.communicate()[1]
+      self.assertIn('not enough operands', response.decode())
+    else:
+      fail('rpncalc.py not found.')
+
+  def test_command_igg_err2(self):
+    '''
+    Tests indiegogo error scenario two.
+    '''
+    if os.path.isfile('rpncalc.py'):
+      command = subprocess.Popen(
+          ['python', 'rpncalc.py', 'a b +'],
+          stdout=subprocess.PIPE,
+          stderr=subprocess.PIPE
+      )
+      response = command.communicate()[1]
+      self.assertIn('numbers and operators', response.decode())
+    else:
+      fail('rpncalc.py not found.')
+
 
 def test_generator(expression, value):
+  '''
+  Generates additional test cases for a particular expression and value, used below.
+  '''
   def test(self):
     from rpncalc import calculator
-    print('testing expression %s' % (expression,))
     self.assertEqual(calculator(expression), Decimal(value))
   return test
 
 if __name__ == '__main__':
+  # generate test cases for each expression in TEST_EXPRESSIONS before running tests
   for expression in TEST_EXPRESSIONS:
     test_name = '_'.join(['test', expression[0]])
     test = test_generator(expression[1], expression[2])
